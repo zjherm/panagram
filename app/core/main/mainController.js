6026e45a04062982f635c5708590165d47d73e76
zherman <zherman@renovateamerica.com>
2017-01-31 15:39:49 -0800

    asdf

--- app/core/main/mainController.js
+++ app/core/main/mainController.js
@@ -11,15 +11,25 @@
 
 		vm.phrase = '';
 		vm.results = [];
+		vm.letter = '';
 
 		vm.getPhrases = function () {
 			console.log('inside getPhrases');
-			$http.get("/query?q=" + vm.phrase)
+			$http.get("/query?q=" + vm.phrase + "&filter=" + vm.letter)
 				.then(function (response) {
 					var time = response.config.responseTimestamp - response.config.requestTimestamp;
 					vm.responseTime = time/1000;
 					vm.results = response.data.results;
 				});
+		};
+		vm.isPalindrome = function(word) {
+		  if(word.length <= 1) return true;
+		  for(var i=0;i<word.length/2;i++) {
+		    if(word[i] !== word[word.length-1-i]) {
+		      return false;
+		    }
+		  }
+		  return true;
 		};
 	}
 })();
