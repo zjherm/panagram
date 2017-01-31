f37d401bd6bee9dff451f8664b1903be7e200552
Ned Stuart <nstuart@renovateamerica.com>
2017-01-31 15:52:51 -0800

    fwef

--- app/core/main/mainController.js
+++ app/core/main/mainController.js
@@ -13,6 +13,22 @@
 		vm.results = [];
 		vm.letter = '';
 
+		vm.getRandomWord = function() {
+			$http.get("/random_word")
+				.then(function (response) {
+					vm.phrase = response.data.word;
+					vm.getPhrases();
+				});
+		}
+
+		vm.isPrime = function(number) {
+			var start = 2;
+			while (start <= Math.sqrt(number)) {
+				if (number % start++ < 1) return false;
+			}
+			return number > 1;
+		}
+
 		vm.getPhrases = function () {
 			console.log('inside getPhrases');
 			$http.get("/query?q=" + vm.phrase + "&filter=" + vm.letter)
@@ -20,6 +36,7 @@
 					var time = response.config.responseTimestamp - response.config.requestTimestamp;
 					vm.responseTime = time/1000;
 					vm.results = response.data.results;
+					vm.numResults = vm.results.length;
 				});
 		};
 		vm.isPalindrome = function(word) {
@@ -31,5 +48,8 @@
 		  }
 		  return true;
 		};
+
+
+		vm.getRandomWord();
 	}
 })();
